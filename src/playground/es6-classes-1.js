4b7629a6b2e53d38a8acdee71f8c41c4743a2462
Lucas Candido <lucascandido.ti@gmail.com>
2019-06-07 16:27:21 -0300

    ES6 Classes Part II

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -11,15 +11,57 @@ class Person {
     }
 
     getDescription(){
-        return `${ this.name } is ${ this.age } year(s) old!`;
+        return `${ this.name } is ${ this.age } year(s) old.`;
     }
 }
 
-const me = new Person('Lucas Candido', 23);
+class Student extends Person{
+
+    constructor(name, age, major){
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor(){
+        return !!this.major;
+    }
+
+    getDescription(){
+        let description = super.getDescription();
+
+        if(this.hasMajor()){
+            description += ` Their major is ${this.major}.`
+        }
+        return description;
+    }
+}
+
+
+class Traveler extends Person{
+
+    constructor(name, age, location){
+        super(name, age);
+        this.location = location;
+    }
+
+    getGretting(){
+        let gretting = super.getGretting();
+        if(this.location){
+            gretting += ` I'm visiting from ${this.location}`
+        }
+        return gretting;
+    }
+}
+
+
+
+const me = new Traveler('Lucas Candido', 23, 'São Paulo');
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Traveler();
 console.log(other.getDescription());
+
+
 
 
 const appRoot = document.getElementById("app");
@@ -27,8 +69,8 @@ const appRoot = document.getElementById("app");
 const renderTemplate = () => {
     const template = (
         <div>
-            <p>{me.getDescription()}</p>
-            <p>{other.getDescription()}</p>
+            <p>{me.getGretting()}</p>
+            <p>{other.getGretting()}</p>
         </div>
         
     );
